3e426808b4f5a92162c0fc49d470f38ceca84903
izumin5210 <masayuki@izumin.info>
2014-10-09 21:27:42 +0900

    接続，切断時の通知を変更
    * ToastをCroutonに変更
    * Connected → READYの間はProgressDialogを表示するようにした

--- app/src/main/java/info/izumin/android/tlperc/ui/TLPercActivity.java
+++ app/src/main/java/info/izumin/android/tlperc/ui/TLPercActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.amalgam.app.ProgressDialogFragment;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 import com.uxxu.konashi.lib.KonashiActivity;
 import com.uxxu.konashi.lib.KonashiErrorReason;
 import com.uxxu.konashi.lib.KonashiEvent;
 
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
 import info.izumin.android.tlperc.R;
 import info.izumin.android.tlperc.model.BusProvider;
 import info.izumin.android.tlperc.model.TLPercKonashiObserver;
@@ -17,16 +20,18 @@ import info.izumin.android.tlperc.model.TLPercKonashiObserver;
 public class TLPercActivity extends KonashiActivity {
     public static final String TAG = TLPercActivity.class.getSimpleName();
 
+    private static final String DIALOG_FRAGMENT = "dialog fragment";
+
     private TLPercKonashiObserver mKonashiObserver;
+    private ProgressDialogFragment mConnectingProcessDialog;
+    private Crouton mConnectedToast, mDisconnectedToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tlperc);
+        initialize();
         transitionFindKonashiFragment();
-        Bus bus = BusProvider.getInstance();
-        mKonashiObserver = new TLPercKonashiObserver(this, bus);
-        bus.register(this);
     }
 
     @Override
@@ -40,11 +45,17 @@ public class TLPercActivity extends KonashiActivity {
     public void onKonashiCallback(KonashiEvent event) {
         Log.d(TAG, event.name());
         switch(event) {
+            case CONNECTED:
+                mConnectingProcessDialog.show(getFragmentManager(), DIALOG_FRAGMENT);
+                break;
             case READY:
+                mConnectedToast.show();
+                mConnectingProcessDialog.dismiss();
                 transitionPercussionFragment();
                 break;
             case DISCONNECTED:
-                Toast.makeText(this, "接続切れたでｗ", Toast.LENGTH_LONG).show();
+                mConnectedToast.cancel();
+                mDisconnectedToast.show();
                 getFragmentManager().popBackStack();
                 break;
         }
@@ -53,6 +64,15 @@ public class TLPercActivity extends KonashiActivity {
     @Subscribe
     public void onKonashiError(KonashiErrorReason errorReason) {
         Log.d(TAG, errorReason.name());
+    }
+
+    private void initialize() {
+        Bus bus = BusProvider.getInstance();
+        bus.register(this);
+        mKonashiObserver = new TLPercKonashiObserver(this, bus);
+        mConnectingProcessDialog = ProgressDialogFragment.newInstance("接続中…", "Konashiに接続しています", true);
+        mConnectedToast = Crouton.makeText(this, "接続できたでｗ", Style.INFO);
+        mDisconnectedToast = Crouton.makeText(this, "接続切れたでｗ", Style.ALERT);
     }
 
     private void transitionFindKonashiFragment() {
